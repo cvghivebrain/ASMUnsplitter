9f9b0ed17dffc93d4c600382549753a6baff012a
cvghivebrain <66194501+cvghivebrain@users.noreply.github.com>
2021-08-11 22:33:49 +0100

    Rewrite

--- Unsplit.dpr
+++ Unsplit.dpr
@@ -3,11 +3,14 @@ program PUnsplit;
 {$APPTYPE CONSOLE}
 
 uses
-  SysUtils, StrUtils;
+  Classes, SysUtils, StrUtils;
 
 var
-  infile, outfile, exfile: textfile;
-  actualline, cleanline, copylabel, exline: string;
+  buffer: TStringList;
+  currentline, copylabel: string;
+  i: integer;
+
+label endnow, nextline, loopline;
 
 { Get part of a string using a delimiter. }
 function Explode(s, d: string; n: integer): string;
@@ -38,60 +41,60 @@ begin
     end;
 end;
 
+{ Tidies a line. }
+function CleanLine(s: string): string;
+begin
+  s := Explode(s,';',0); // Strip comments.
+  s := ReplaceStr(s,#9,' '); // Replace tabs with spaces.
+  s := ReplaceStr(s,#39,'"'); // Replace single quotes with double quotes.
+  while AnsiPos('  ',s) <> 0 do s := ReplaceStr(s,'  ',' '); // Replace double spaces with single.
+  Result := s;
+end;
+
+{ Inserts the contents of a file to the main file buffer. }
+procedure InsertFile(filestr: string; linenum: integer);
+var exfile: textfile;
+  j: integer;
+  s: string;
+begin
+  j := 0;
+  AssignFile(exfile,ExtractFilePath(ParamStr(1))+filestr); // Open external file (read only).
+  Reset(exfile);
+  while not eof(exfile) do
+    begin
+    ReadLn(exfile,s); // Read from external file.
+    buffer.Insert(linenum+j,s); // Write to buffer.
+    inc(j); // Next line (in buffer).
+    end;
+  CloseFile(exfile); // Close external file.
+end;
+
 begin
   { Program start }
-  if ParamCount = 1 then
-    begin
-    AssignFile(infile, ParamStr(1)); // open input file (read only)
-    Reset(infile);                    
-    AssignFile(outfile, ParamStr(1)+'_unsplit'); // open output file (read/write)
-    ReWrite(outfile);
-    while not eof(infile) do
-      begin
-      ReadLn(infile,actualline); // read line from file
-      if AnsiPos('include',actualline) <> 0 then // check if line contains "include"
-        begin
-        cleanline := Explode(actualline,';',0); // strip comments
-        cleanline := StringReplace(cleanline,#9,' ',[rfReplaceAll]); // replace tabs with spaces
-        if AnsiPos(':',cleanline) <> 0 then // check for label
-          begin
-          copylabel := Explode(cleanline,':',0); // save label
-          cleanline := Explode(cleanline,':',1); // remove label
-          end
-        else copylabel := '';
-        cleanline := Trim(cleanline); // remove excess spaces
-        if AnsiPos('include',cleanline) = 1 then
-          begin
-          { Include external file }
-          if copylabel <> '' then WriteLn(outfile,copylabel+':'); // write label
-          Delete(cleanline,1,7); // remove "include"
-          cleanline := Trim(cleanline); // remove excess spaces again   
-          cleanline := StringReplace(cleanline,'"','',[rfReplaceAll]); // remove quotes
-          cleanline := ExtractFilePath(ParamStr(1))+cleanline; // add full file path
-          if FileExists(cleanline) = true then // check if file exists
-            begin
-            AssignFile(exfile, cleanline); // open external file (read only)
-            Reset(exfile);
-            while not eof(exfile) do
-              begin
-              ReadLn(exfile,exline); // read from external file
-              WriteLn(outfile,exline); // write to output file
-              end;
-            CloseFile(exfile); // close external file
-            end;
 
-          end
-        else WriteLn(outfile,actualline); // "include" was false positive, copy line as-is instead
-        end
-      else WriteLn(outfile,actualline); // no "include" found, copy line as-is
-      end;
-    CloseFile(infile);
-    CloseFile(outfile);
-    end
-  else
-    begin
-    WriteLn('ASM Unsplitter by Hivebrain');
-    ReadLn;
-    end;
+  if ParamStr(1) = '' then goto endnow; // End program if run without parameters.
 
+  buffer := TStringList.Create;
+  buffer.LoadFromFile(ParamStr(1)); // Copy main asm file to memory.
+
+  i := 0;
+
+  loopline:
+  if AnsiPos('include',buffer[i]) = 0 then goto nextline; // Check if line contains "include".
+  currentline := CleanLine(buffer[i]); // Get line without tabs, comments etc.
+  if AnsiPos(' include "',currentline) = 0 then goto nextline; // Final check for positive "include".
+  copylabel := Trim(Explode(currentline,' include "',0)); // Get label (if there is one).
+  if (AnsiPos(':',copylabel) = 0) and (copylabel <> '') then copylabel := copylabel+':'; // Add colon if needed. 
+  buffer[i] := copylabel; // Replace line with label only.
+  InsertFile(Explode(currentline,'"',1),i+1); // Insert external file at current line.
+
+  nextline:
+  inc(i); // Next line.
+  if i < buffer.Count then goto loopline; // Repeat if there are more lines.
+  
+  if FileExists(ParamStr(1)+'_unsplit') then DeleteFile(ParamStr(1)+'_unsplit');
+  buffer.SaveToFile(ParamStr(1)+'_unsplit'); // Save output file.
+  buffer.Free;
+
+  endnow:
 end.
