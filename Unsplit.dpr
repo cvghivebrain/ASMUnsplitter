73cc16f46290146f657fb3a89c5cf540c88091d1
cvghivebrain <66194501+cvghivebrain@users.noreply.github.com>
2023-04-16 22:52:15 +0100

    Rewrite 2023
    Updated Explode function.
    More efficient line replacement (thanks to ChatGPT).
    Added log file.
    Doesn't crash if included file is missing.

--- Unsplit.dpr
+++ Unsplit.dpr
@@ -6,38 +6,27 @@ uses
   Classes, SysUtils, StrUtils;
 
 var
-  buffer: TStringList;
-  currentline, copylabel: string;
+  buffer, incbuffer: TStringList;
+  currentline, copylabel, incname: string;
   i: integer;
+  logfile: textfile;
 
 label endnow, nextline, loopline;
 
 { Get part of a string using a delimiter. }
-function Explode(s, d: string; n: integer): string;
-var n2: integer;
+function Explode(str, delimiter: string; n: integer): string; // Get substring from string using delimiter.
 begin
-  if (AnsiPos(d,s) = 0) and ((n = 0) or (n = -1)) then result := s // Output full string if delimiter not found.
+  if (AnsiPos(delimiter,str) = 0) and ((n = 0) or (n = -1)) then result := str // Output full string if delimiter not found.
   else
     begin
-    if n > -1 then // Check for negative substring.
+    str := str+delimiter;
+    while n > 0 do
       begin
-      s := s+d;
-      n2 := n;
-      end
-    else
-      begin
-      d := AnsiReverseString(d);
-      s := AnsiReverseString(s)+d; // Reverse string for negative.
-      n2 := (n*-1)-1;
+      Delete(str,1,AnsiPos(delimiter,str)+Length(delimiter)-1); // Trim earlier substrings and delimiters.
+      dec(n);
       end;
-    while n2 > 0 do
-      begin
-      Delete(s,1,AnsiPos(d,s)+Length(d)-1); // Trim earlier substrings and delimiters.
-      dec(n2);
-      end;
-    Delete(s,AnsiPos(d,s),Length(s)-AnsiPos(d,s)+1); // Trim later substrings and delimiters.
-    if n < 0 then s := AnsiReverseString(s); // Un-reverse string if negative.
-    result := s;
+    Delete(str,AnsiPos(delimiter,str),Length(str)-AnsiPos(delimiter,str)+1); // Trim later substrings and delimiters.
+    result := str;
     end;
 end;
 
@@ -52,24 +41,6 @@ begin
   Result := s;
 end;
 
-{ Inserts the contents of a file to the main file buffer. }
-procedure InsertFile(filestr: string; linenum: integer);
-var exfile: textfile;
-  j: integer;
-  s: string;
-begin
-  j := 0;
-  AssignFile(exfile,ExtractFilePath(ParamStr(1))+filestr); // Open external file (read only).
-  Reset(exfile);
-  while not eof(exfile) do
-    begin
-    ReadLn(exfile,s); // Read from external file.
-    buffer.Insert(linenum+j,s); // Write to buffer.
-    inc(j); // Next line (in buffer).
-    end;
-  CloseFile(exfile); // Close external file.
-end;
-
 begin
   { Program start }
 
@@ -77,6 +48,9 @@ begin
 
   buffer := TStringList.Create;
   buffer.LoadFromFile(ParamStr(1)); // Copy main asm file to memory.
+  AssignFile(logfile,'Unsplit.log'); // Create logfile.
+  ReWrite(logfile);
+  WriteLn(logfile,IntToStr(buffer.Count)+' lines in original file.');
 
   i := 0;
 
@@ -84,18 +58,37 @@ begin
   if AnsiPos('include',buffer[i]) = 0 then goto nextline; // Check if line contains "include".
   currentline := CleanLine(buffer[i]); // Get line without tabs, comments etc.
   if AnsiPos(' include "',currentline) = 0 then goto nextline; // Final check for positive "include".
+
+  incbuffer := TStringList.Create;
+  incname := Explode(currentline,'"',1);
+  try
+    incbuffer.LoadFromFile(incname); // Get contents of included file.
+    WriteLn(logfile,incname+' opened.');
+    WriteLn(logfile,IntToStr(incbuffer.Count)+' lines in included file.');
+  except
+    WriteLn(logfile,incname+' failed to load.');
+  end;
   copylabel := Trim(Explode(currentline,' include "',0)); // Get label (if there is one).
-  if copylabel <> '' then copylabel := copylabel+':'; // Add colon to label.
-  buffer[i] := copylabel; // Replace line with label only.
-  InsertFile(Explode(currentline,'"',1),i+1); // Insert external file at current line.
+  if copylabel <> '' then
+    begin
+    copylabel := copylabel+':'; // Add colon to label.
+    buffer.Insert(i,copylabel); // Add line with label only.
+    buffer[i+1] := incbuffer.Text; // Replace original line with included file.
+    end
+  else buffer[i] := incbuffer.Text;
+  buffer.SetText(PChar(buffer.Text)); // Reset buffer to accomodate new lines.
+  incbuffer.Free;
+  WriteLn(logfile,IntToStr(buffer.Count)+' lines in updated file.');
 
   nextline:
   inc(i); // Next line.
   if i < buffer.Count then goto loopline; // Repeat if there are more lines.
   
-  if FileExists(ParamStr(1)+'_unsplit') then DeleteFile(ParamStr(1)+'_unsplit');
-  buffer.SaveToFile(ParamStr(1)+'_unsplit'); // Save output file.
+  if FileExists(ParamStr(1)+'.unsplit') then DeleteFile(ParamStr(1)+'.unsplit');
+  buffer.SaveToFile(ParamStr(1)+'.unsplit'); // Save output file.
   buffer.Free;
+  WriteLn(logfile,IntToStr(i)+' lines written.');
+  CloseFile(logfile);
 
   endnow:
 end.
